61f388a1c968997cc80e1287eaadb6f5e1d9c772
Galaxian <manognachamarthi@gmail.com>
2022-06-15 12:18:27 +0530

    changed some code

--- Prototype 1/Assets/Scripts/PlayerController.cs
+++ Prototype 1/Assets/Scripts/PlayerController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 20f;
+    [SerializeField] float speed = 20f;
     // Start is called before the first frame update
     void Start()
     {
