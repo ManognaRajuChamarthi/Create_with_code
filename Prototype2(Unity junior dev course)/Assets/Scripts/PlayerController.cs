09b06d93a2bbd77b4bf514ad726c5b8d3d2f1efe
Galaxian <manognachamarthi@gmail.com>
2022-06-18 14:25:26 +0530

    added player bounds

--- Prototype2(Unity junior dev course)/Assets/Scripts/PlayerController.cs
+++ Prototype2(Unity junior dev course)/Assets/Scripts/PlayerController.cs
@@ -17,7 +17,14 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-     
+        if (transform.position.x > 15f)
+        {
+            transform.position = new Vector3(15, transform.position.y, transform.position.z);
+        }
+        else if(transform.position.x < -15f)
+        {
+            transform.position = new Vector3(-15, transform.position.y, transform.position.z);
+        }
         float horizontal = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * Time.deltaTime * speed * horizontal);
     }
