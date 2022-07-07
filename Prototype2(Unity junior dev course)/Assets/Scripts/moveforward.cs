66854ba2290e9c9f87b2ce6656463196b6567d97
Galaxian <manognachamarthi@gmail.com>
2022-07-07 12:21:25 +0530

    converted the hardcoded numbers to variables to make it easy to edit form the inspector

--- Prototype2(Unity junior dev course)/Assets/Scripts/moveforward.cs
+++ Prototype2(Unity junior dev course)/Assets/Scripts/moveforward.cs
@@ -6,6 +6,8 @@ public class moveforward : MonoBehaviour
 {
     //Variables
     public float speed = 40.0f;
+    public int lowerBound = -5;
+    public int upperBound = 20;
 
     // Start is called before the first frame update
     void Start()
@@ -17,7 +19,7 @@ public class moveforward : MonoBehaviour
     void Update()
     {
         transform.Translate(Vector3.forward * Time.deltaTime * speed);
-        if (transform.position.z>17 || transform.position.z < -2)
+        if (transform.position.z>upperBound || transform.position.z < lowerBound)
         {
             Destroy(gameObject);
         }
