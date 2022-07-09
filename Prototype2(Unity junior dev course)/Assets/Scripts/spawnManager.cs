fc0efe2c0d2d7fb69d1601f65e15109bf67dba38
Galaxian <manognachamarthi@gmail.com>
2022-07-09 12:20:03 +0530

    made a spawn manager system

---
+++ Prototype2(Unity junior dev course)/Assets/Scripts/spawnManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawnManager : MonoBehaviour
+{
+    //Variables
+    public GameObject[] animals;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int index = Random.Range(0, animals.Length-1);
+        if (Input.GetKey(KeyCode.S))
+        {
+            Instantiate(animals[index], new Vector3(Random.Range(-15,15), 0,19),animals[index].transform.rotation);
+        }
+    }
+}
